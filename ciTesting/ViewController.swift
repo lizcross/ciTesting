31cc2056a3e1393bd2b9d87d6249ebbc8f9c323b
Liz Cross <elizabeth.cross@gmail.com>
2018-04-09 21:09:31 -0700

    Initial Commit

---
+++ ciTesting/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ciTesting
+//
+//  Created by Liz Cross on 2018-04-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
